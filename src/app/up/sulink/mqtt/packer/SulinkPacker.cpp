3fd35b05eb635b384eca7c06e3f6caf399bf68f9
chenxiaosong <chenxiaosong@unisound.com>
2020-12-02 17:41:37 +0800

    add more log

--- src/app/up/sulink/mqtt/packer/SulinkPacker.cpp
+++ src/app/up/sulink/mqtt/packer/SulinkPacker.cpp
@@ -239,50 +239,50 @@ SulinkMessageRecvLiftInfo* SulinkPacker :: unpackRecvLiftInfo(const string& raw_
         if (jspace.Get("id", svalue) != true) {
             //LOGE(SULINK_PACKER_TAG, "parse space id failed, i=%d", i);
             //return NULL;
-            err_message = "parse space id failed, i=" + to_string(i);
+            err_message = "parse spaces.id failed, i:" + to_string(i);
             goto L_ERROR;
         }
         space.id(svalue);
+        if (jspace.Get("elevatorSpaceType", svalue) != true) {
+            //LOGE(SULINK_PACKER_TAG, "parse space elevatorSpaceType failed, i=%d", i);
+            //return NULL;
+            err_message = "parse spaces.elevatorSpaceType failed, id:" + space.id();
+            goto L_ERROR;
+        }
+        space.elevatorSpaceType(svalue);
         if (jspace.Get("pid", svalue) != true) {
             //LOGE(SULINK_PACKER_TAG, "parse space pid failed, i=%d", i);
             //return NULL;
-            err_message = "parse space pid failed, i=" + to_string(i);
+            err_message = "parse spaces.pid failed, id:" + space.id() + " type:" + space.elevatorSpaceType();
             goto L_ERROR;
         }
         space.pid(svalue);
         if (jspace.Get("spaceName", svalue) != true) {
             //LOGE(SULINK_PACKER_TAG, "parse space spaceName failed, i=%d", i);
             //return NULL;
-            err_message = "parse space spaceName failed, i=" + to_string(i);
+            err_message = "parse spaces.spaceName failed, id:" + space.id() + " type:" + space.elevatorSpaceType();
             goto L_ERROR;
         }
         space.spaceName(svalue);
-        if (jspace.Get("elevatorSpaceType", svalue) != true) {
-            //LOGE(SULINK_PACKER_TAG, "parse space elevatorSpaceType failed, i=%d", i);
-            //return NULL;
-            err_message = "parse space elevatorSpaceType failed, i=" + to_string(i);
-            goto L_ERROR;
-        }
-        space.elevatorSpaceType(svalue);
         if (jspace.Get("elevatorSpaceNo", svalue) != true) {
             //LOGE(SULINK_PACKER_TAG, "parse space elevatorSpaceNo failed, i=%d", i);
             //return NULL;
-            err_message = "parse space elevatorSpaceNo failed, i=" + to_string(i);
+            err_message = "parse spaces.elevatorSpaceNo failed, id:" + space.id() + " type:" + space.elevatorSpaceType();
             goto L_ERROR;
         }
         space.elevatorSpaceNo(svalue);
         if (jspace.Get("clusterControllerIds", svalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse space clusterControllerIds failed, i=%d", i);
+            LOGW(SULINK_PACKER_TAG, "parse spaces.clusterControllerIds failed, id:%s type:%s", space.id().c_str(), space.elevatorSpaceType().c_str());
         } else {
             space.clusterControllerIds(svalue);
         }
         if (jspace.Get("floorLocation", svalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse space floorLocation failed, i=%d", i);
+            LOGW(SULINK_PACKER_TAG, "parse spaces.floorLocation failed, id:%s type:%s", space.id().c_str(), space.elevatorSpaceType().c_str());
         } else {
             space.floorLocation(svalue);
         }
         if (jspace.Get("timestamp", lvalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse space timestamp failed, i=%d", i);
+            LOGW(SULINK_PACKER_TAG, "parse spaces.timestamp failed, id:%s type:%s", space.id().c_str(), space.elevatorSpaceType().c_str());
         } else {
             space.timestamp(lvalue);
         }
@@ -299,7 +299,7 @@ SulinkMessageRecvLiftInfo* SulinkPacker :: unpackRecvLiftInfo(const string& raw_
     {
         //LOGE(SULINK_PACKER_TAG, "parse brandName failed");
         //return NULL;
-        err_message = "parse brandName failed";
+        err_message = "parse brandInfo.brandName failed";
         goto L_ERROR;
     }
     info.brandInfo().brandName(svalue);
@@ -307,62 +307,62 @@ SulinkMessageRecvLiftInfo* SulinkPacker :: unpackRecvLiftInfo(const string& raw_
     {
         //LOGE(SULINK_PACKER_TAG, "parse brandCode failed");
         //return NULL;
-        err_message = "parse brandCode failed";
+        err_message = "parse brandInfo.brandCode failed";
         goto L_ERROR;
     }
     info.brandInfo().brandCode(svalue);
     if (Jbrand_info.Get("intranetUrl", svalue) != true)
     {
-        LOGW(SULINK_PACKER_TAG, "parse intranetUrl failed");
+        LOGW(SULINK_PACKER_TAG, "parse brandInfo.intranetUrl failed");
     } else {
         info.brandInfo().intranetUrl(svalue);
     }
     if (Jbrand_info.Get("appId", svalue) != true)
     {
-        LOGW(SULINK_PACKER_TAG, "parse appId failed");
+        LOGW(SULINK_PACKER_TAG, "parse brandInfo.appId failed");
     } else {
         info.brandInfo().appId(svalue);
     }
     if (Jbrand_info.Get("appSecret", svalue) != true)
     {
-        LOGW(SULINK_PACKER_TAG, "parse appSecret failed");
+        LOGW(SULINK_PACKER_TAG, "parse brandInfo.appSecret failed");
     } else {
         info.brandInfo().appSecret(svalue);
     }
     if (Jbrand_info.Get("licence", svalue) != true)
     {
-        LOGW(SULINK_PACKER_TAG, "parse licence failed");
+        LOGW(SULINK_PACKER_TAG, "parse brandInfo.licence failed");
     } else {
         info.brandInfo().licence(svalue);
     }
     if (Jbrand_info.Get("schema", svalue) != true)
     {
-        LOGW(SULINK_PACKER_TAG, "parse schema failed");
+        LOGW(SULINK_PACKER_TAG, "parse brandInfo.schema failed");
     } else {
         info.brandInfo().schema(svalue);
     }
     if (Jbrand_info.Get("clusterControllers", jclusters) != true)
     {
-        LOGW(SULINK_PACKER_TAG, "parse clusterControllers failed");
+        LOGW(SULINK_PACKER_TAG, "parse brandInfo.clusterControllers failed");
     } else {
         CJsonObject jcluster;
         LiftInfoCluster cluster;
         for (int i = 0; i < jclusters.GetArraySize(); i++) {
             jclusters.Get(i, jcluster);
-            if (jcluster.Get("controllerIp", svalue) != true) {
-                //LOGE(SULINK_PACKER_TAG, "parse cluster ip failed, i=%d", i);
-                //return NULL;
-                err_message = "parse cluster ip failed, i=" + to_string(i);
-                goto L_ERROR;
-            }
-            cluster.controllerIp(svalue);
             if (jcluster.Get("controllerId", svalue) != true) {
                 //LOGE(SULINK_PACKER_TAG, "parse cluster id failed, i=%d", i);
                 //return NULL;
-                err_message = "parse cluster id failed, i=" + to_string(i);
+                err_message = "parse clusterControllers.id failed, i:" + to_string(i);
                 goto L_ERROR;
             }
             cluster.controllerId(svalue);
+            if (jcluster.Get("controllerIp", svalue) != true) {
+                //LOGE(SULINK_PACKER_TAG, "parse cluster ip failed, i=%d", i);
+                //return NULL;
+                err_message = "parse clusterControllers.ip failed, id:" + cluster.controllerId();
+                goto L_ERROR;
+            }
+            cluster.controllerIp(svalue);
             info.brandInfo().clusterControllers().push_back(cluster);
         }
     }
@@ -379,44 +379,44 @@ SulinkMessageRecvLiftInfo* SulinkPacker :: unpackRecvLiftInfo(const string& raw_
         if (jdevice.Get("deviceCode", svalue) != true) {
             //LOGE(SULINK_PACKER_TAG, "parse device deviceCode failed");
             //return NULL;
-            err_message = "parse device deviceCode failed";
+            err_message = "parse childDevices.deviceCode failed, i:" + to_string(i);
             goto L_ERROR;
         }
         device.deviceCode(svalue);
         if (jdevice.Get("workMode", ivalue) != true) {
             //LOGE(SULINK_PACKER_TAG, "parse device workMode failed");
             //return NULL;
-            err_message = "parse device workMode failed";
+            err_message = "parse childDevices.workMode failed, deviceCode:" + device.deviceCode();
             goto L_ERROR;
         }
         device.workMode(ivalue);
         if (jdevice.Get("floorMap", svalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse device floorMap failed");
+            LOGW(SULINK_PACKER_TAG, "parse childDevices.floorMap failed, deviceCode:%s", device.deviceCode().c_str());
         } else {
             device.floorMap(svalue);
         }
         if (jdevice.Get("publicFloor", svalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse device publicFloor failed");
+            LOGW(SULINK_PACKER_TAG, "parse childDevices.publicFloor failed, deviceCode:%s", device.deviceCode().c_str());
         } else {
             device.publicFloor(svalue);
         }
         if (jdevice.Get("curFloor", svalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse device curFloor failed");
+            LOGW(SULINK_PACKER_TAG, "parse childDevices.curFloor failed, deviceCode:%s", device.deviceCode().c_str());
         } else {
             device.curFloor(svalue);
         }
         if (jdevice.Get("liftId", svalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse device liftId failed");
+            LOGW(SULINK_PACKER_TAG, "parse childDevices.liftId failed, deviceCode:%s", device.deviceCode().c_str());
         } else {
             device.liftId(svalue);
         }
         if (jdevice.Get("hallNo", svalue) != true) {
-            LOGW(SULINK_PACKER_TAG, "parse device hallNo failed");
+            LOGW(SULINK_PACKER_TAG, "parse childDevices.hallNo failed, deviceCode:%s", device.deviceCode().c_str());
         } else {
             device.hallNo(svalue);
         }
         if (jdevice.Get("timestamp", lvalue) != true) {
-            err_message = "parse device timestamp failed";
+            err_message = "parse childDevices.timestamp failed, deviceCode:%s" + device.deviceCode();
             goto L_ERROR;
         }
         device.timestamp(lvalue);
