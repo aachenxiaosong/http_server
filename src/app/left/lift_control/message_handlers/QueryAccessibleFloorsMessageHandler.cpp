91a0eb376b83c2bf7931b2aa1f114218794f2663
root <root@localhost.localdomain>
2021-01-29 17:20:13 +0800

    v3.4.3 remove repeated floor in queryAccessibleFloors

--- src/app/left/lift_control/message_handlers/QueryAccessibleFloorsMessageHandler.cpp
+++ src/app/left/lift_control/message_handlers/QueryAccessibleFloorsMessageHandler.cpp
@@ -2,6 +2,7 @@
 #include "SulinkLiftInitData.hpp"
 #include "UniLog.hpp"
 #include "boost/tokenizer.hpp"
+#include <set>
 
 #define QUERY_ACCESSIBLE_FLOORS_MSG_HANDLER_TAG getName().c_str()
 
@@ -18,6 +19,7 @@ LiftCtrlMessageRsp* QueryAccessibleFloorsMessageHandler :: handle(const LiftCtrl
     if (request.type() != MSG_LIFT_CTRL_QUERY_ACCESSIBLE_FLOORS_REQ) {
         return NULL;
     }
+    set<string> maccessible_floors;
     string accessible_floors = "";
     LiftCtrlMessageQueryAccessibleFloorsReq& req = (LiftCtrlMessageQueryAccessibleFloorsReq &)request;
     LiftCtrlMessageQueryAccessibleFloorsRsp rsp;
@@ -26,29 +28,36 @@ LiftCtrlMessageRsp* QueryAccessibleFloorsMessageHandler :: handle(const LiftCtrl
     for (auto home_id : req.authorizedHomeIds()) {
         string floor = SulinkLiftInitData :: getFloorNoBySpaceId(home_id);
         if (floor.empty()) {
-	  LOGE(QUERY_ACCESSIBLE_FLOORS_MSG_HANDLER_TAG, "floor number not found for home_id" + home_id);
+            LOGE(QUERY_ACCESSIBLE_FLOORS_MSG_HANDLER_TAG, "floor number not found for home_id" + home_id);
         } else {
-          rsp.accessibleFloors().push_back(floor);
-          accessible_floors += floor;
-          accessible_floors += " ";
+            // rsp.accessibleFloors().push_back(floor);
+            // accessible_floors += floor;
+            // accessible_floors += " ";
+            maccessible_floors.insert(floor);
         }
     }
     //part2: public floors
     char_separator<char> sep(",");
     tokenizer<char_separator<char> > tok(SulinkLiftInitData :: getDevicePubFloors(req.deviceCode()), sep);
     for (auto substr :  tok) {
-        rsp.accessibleFloors().push_back(substr);
-        accessible_floors += substr;
-        accessible_floors += " ";
+        // rsp.accessibleFloors().push_back(substr);
+        // accessible_floors += substr;
+        // accessible_floors += " ";
+        maccessible_floors.insert(substr);
     }
-    if (rsp.accessibleFloors().empty()) {
+    if (maccessible_floors.empty()) {
         rsp.retcode(RETCODE_ERROR);
         rsp.msg("no accessible floor found");
-	LOGE(QUERY_ACCESSIBLE_FLOORS_MSG_HANDLER_TAG, "no accessible floor found");
+        LOGE(QUERY_ACCESSIBLE_FLOORS_MSG_HANDLER_TAG, "no accessible floor found");
     } else {
+        for (auto floor : maccessible_floors) {
+            rsp.accessibleFloors().push_back(floor);
+            accessible_floors += floor;
+            accessible_floors += " ";
+        }
         rsp.retcode(RETCODE_OK);
         rsp.msg("OK");
-	LOGT(QUERY_ACCESSIBLE_FLOORS_MSG_HANDLER_TAG, "get accessible floor " + accessible_floors);
+        LOGT(QUERY_ACCESSIBLE_FLOORS_MSG_HANDLER_TAG, "get accessible floor " + accessible_floors);
     }
     return new LiftCtrlMessageQueryAccessibleFloorsRsp(rsp);
 }
